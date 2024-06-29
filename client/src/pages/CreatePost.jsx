47f69001f46c14588e373adf77bf13f5a1ae84a6
fazil2915 <fazil2915@gmail.com>
2024-06-29 13:31:49 +0530

    up:autoPromptfetch

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+
+import {useState} from "react"
+import { useNavigate } from "react-router-dom"
+import {preview} from "../assets"
+import { FormField,Loader } from "../components"
+import {getRandomPrompt} from "../utils"
+
 
 function CreatePost() {
   return (
---
+++ client/src/utils/index.js
@@ -0,0 +1,9 @@
+import {surpriseMePrompts} from "../components"
+
+export function getRandomPrompt(prompts){
+    const randomIndex=Math.floor(Math.random()*surpriseMePrompts.length);
+    const randomPrompt=surpriseMePrompts[randomIndex];
+
+    if(randomPrompt===prompts) return getRandomPrompt(prompts)
+    return randomPrompt;
+}
